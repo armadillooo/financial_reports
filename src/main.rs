7c291b6880429cf660c4ce2be44be16c525a09d5
armadillooo <abchan23@gmail.com>
2022-05-30 22:44:55 +0900

    introduce logging by tracing crates

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,26 @@
 mod api;
 mod session;
 
-use std::net::SocketAddr;
+use std::{net::SocketAddr, str::FromStr};
+
+use dotenvy::{self, dotenv};
 
 use api::api_routes;
 
 #[tokio::main]
 async fn main() {
+    // .envファイルを読み込み
+    dotenv().ok();
+    // Default Logger初期化
+    tracing_subscriber::fmt::init();
+
     let app = api_routes();
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = dotenvy::var("SOCKET_ADDRESS").unwrap();
+    let addr = SocketAddr::from_str(&addr).unwrap();
 
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
-        .unwrap()
+        .unwrap();
 }
