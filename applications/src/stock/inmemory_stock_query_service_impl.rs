a316e67395625e53ab4e6eb1c81be724ddc816f5
aaa <sample@gmail.com>
2022-12-14 17:02:22 +0900

    StockQueryServiceのパラメータテストを追加

--- applications/src/stock/inmemory_stock_query_service_impl.rs
+++ applications/src/stock/inmemory_stock_query_service_impl.rs
@@ -55,29 +55,28 @@ impl StockQueryService for InmemoryStockQueryServiceImpl {
         } else {
             self.stocks.len()
         };
+        let skip_count = page_index as usize * page_size;
 
-        let iter = self
-            .stocks
-            .to_vec()
-            .into_iter()
-            .filter(find_by_id)
-            .filter(find_by_date_from)
-            .filter(find_by_date_to)
-            .skip(page_index as usize * page_size)
-            .take(page_size);
+        let iter = self.stocks.to_vec().into_iter().filter(find_by_id)
+        .filter(find_by_date_from)
+        .filter(find_by_date_to)
+        .skip(skip_count)
+        .take(page_size);
 
-        Ok(iter.collect::<Vec<StockData>>())
+        let result = iter.collect::<Vec<StockData>>();
+
+        Ok(result)
     }
 }
 
 #[cfg(test)]
 mod test {
-    use crate::stock::{
-        stock_data::StockData, stock_query_parameters::StockQueryParameters,
-        stock_query_service::StockQueryService,
-    };
+    use chrono::NaiveDate;
 
-    use super::InmemoryStockQueryServiceImpl;
+    use crate::stock::{
+        inmemory_stock_query_service_impl::InmemoryStockQueryServiceImpl, stock_data::StockData,
+        stock_query_parameters::StockQueryParameters, stock_query_service::StockQueryService,
+    };
 
     fn setup() -> InmemoryStockQueryServiceImpl {
         InmemoryStockQueryServiceImpl::new()
@@ -97,7 +96,81 @@ mod test {
         }
         service.stocks = stocks;
 
-        assert!(service.find(param).await?.pop().unwrap().stock_id == target_id);
+        let found = service.find(param).await?;
+        
+        assert!(found.len() == 1);
+        assert!(found[0].stock_id == target_id);
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn find_by_date_from() -> anyhow::Result<()> {
+        let mut service = setup();
+        let mut param = StockQueryParameters::new();
+        let target_date = NaiveDate::from_ymd_opt(2022, 7, 12);
+        param.date_from = target_date;
+
+        let mut stocks = Vec::new();
+        for i in 0..4 {
+            stocks.insert(i, StockData::new());
+        }
+        stocks[0].date = NaiveDate::from_ymd_opt(2021, 1, 1).unwrap();
+        stocks[1].date = target_date.unwrap();
+        stocks[2].date = NaiveDate::from_ymd_opt(2022, 8, 30).unwrap();
+        stocks[3].date = NaiveDate::from_ymd_opt(2025, 9, 15).unwrap();
+        service.stocks = stocks.to_vec();
+
+        let found = service.find(param).await?;
+
+        assert!(found == stocks[1..]);
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn find_by_date_to() -> anyhow::Result<()> {
+        let mut service = setup();
+        let mut param = StockQueryParameters::new();
+        let target_date = NaiveDate::from_ymd_opt(2022, 7, 12);
+        param.date_to = target_date;
+
+        let mut stocks = Vec::new();
+        for i in 0..4 {
+            stocks.insert(i, StockData::new());
+        }
+        stocks[0].date = NaiveDate::from_ymd_opt(2021, 1, 1).unwrap();
+        stocks[1].date = target_date.unwrap();
+        stocks[2].date = NaiveDate::from_ymd_opt(2022, 8, 30).unwrap();
+        stocks[3].date = NaiveDate::from_ymd_opt(2025, 9, 15).unwrap();
+        service.stocks = stocks.to_vec();
+
+        let found = service.find(param).await?;
+
+        assert!(found == stocks[..2]);
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn pagenation() -> anyhow::Result<()> {
+        let mut service = setup();
+        let mut param = StockQueryParameters::new();
+        let index = Some(2);
+        let page_size = Some(1);
+        param.page = index;
+        param.size = page_size;
+
+        let mut stocks = Vec::new();
+        for i in 0..3 {
+            stocks.insert(i, StockData::new());
+            stocks[i].stock_id = i.to_string();
+        }
+        service.stocks = stocks;
+
+        let found = service.find(param).await?;
+        assert!(found.len() as i32 == page_size.unwrap());
+        assert!(found[0].stock_id == index.unwrap().to_string());
 
         Ok(())
     }
