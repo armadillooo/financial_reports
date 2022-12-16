863369a1462d4814b14ff570d682e21d1fd61f98
aaa <sample@gmail.com>
2022-12-16 14:22:38 +0900

    会社情報取得APIを仮実装

--- applications/src/stock/company_query_parameters.rs
+++ applications/src/stock/company_query_parameters.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq, Default)]
 pub struct CompanyQueryParameters {
     pub name: Option<String>,
     pub stock_id: Option<String>,
@@ -6,4 +6,11 @@ pub struct CompanyQueryParameters {
     pub industry: Option<String>,
     pub page: Option<i32>,
     pub size: Option<i32>,
+}
+
+impl CompanyQueryParameters {
+    /// コンストラクタ
+    pub fn new() -> Self {
+        Self::default()
+    }
 }
--- presentation/src/stock/stock_controller.rs
+++ presentation/src/stock/stock_controller.rs
@@ -2,7 +2,9 @@ use std::collections::HashMap;
 
 use chrono::NaiveDate;
 
-use applications::stock::{StockQueryParameters, StockQueryService};
+use applications::stock::{
+    CompanyQueryParameters, CompanyQueryService, StockQueryParameters, StockQueryService,
+};
 use axum::{
     extract::{Extension, Path, Query},
     response::IntoResponse,
@@ -14,17 +16,17 @@ use crate::common::{Utility, UtilityImpl};
 
 pub fn stock_cotroller() -> Router {
     Router::new()
-        .route("/stocks/:id", get(get_stocks))
+        .route("/stocks/:stock_id", get(get_stocks))
         .route("/companies", get(get_companies))
 }
 
 async fn get_stocks(
     Extension(utility): Extension<UtilityImpl>,
     Query(queries): Query<HashMap<String, String>>,
-    Path(id): Path<String>,
+    Path(stock_id): Path<String>,
 ) -> impl IntoResponse {
     let mut params = StockQueryParameters::new();
-    params.stock_id = Some(id);
+    params.stock_id = Some(stock_id);
     // クエリパラメータ取得
     params.date_from = if let Some(date) = queries.get("date_from") {
         let Ok(date) = NaiveDate::parse_from_str(date, "%Y-%m-%d") else { return "Err"};
@@ -60,5 +62,26 @@ async fn get_companies(
     Extension(utility): Extension<UtilityImpl>,
     Query(queries): Query<HashMap<String, String>>,
 ) -> impl IntoResponse {
-    unimplemented!()
+    let mut params = CompanyQueryParameters::new();
+    // クエリパラメータ取得
+    params.name = queries.get("name").map(|s| s.to_string());
+    params.stock_id = queries.get("stock_id").map(|s| s.to_string());
+    params.sector = queries.get("sector").map(|s| s.to_string());
+    params.industry = queries.get("industry").map(|s| s.to_string());
+    params.page = if let Some(page) = queries.get("page") {
+        let Ok(page) = page.parse() else { return "Err"};
+        Some(page)
+    } else {
+        None
+    };
+    params.size = if let Some(size) = queries.get("size") {
+        let Ok(page) = size.parse() else { return "Err"};
+        Some(page)
+    } else {
+        None
+    };
+
+    let result = utility.company_query_service().find(params).await.unwrap();
+
+    "Ok"
 }
