167cded4cfb75340d952fea9ad486c677b9de50f
aaa <sample@gmail.com>
2022-12-28 16:50:19 +0900

    企業情報取得サービスのユニットテストを実装

--- applications/src/company/inmemory_company_query_service_impl.rs
+++ applications/src/company/inmemory_company_query_service_impl.rs
@@ -107,43 +107,195 @@ impl CompanyQueryService for InmemoryCompanyQueryServiceImpl {
 
 #[cfg(test)]
 mod tests {
-    #[tokio::test]
-    async fn find_by_name() -> anyhow::Result<()> {
-        unimplemented!()
+    use anyhow::anyhow;
+
+    use crate::company::{
+        CompanyData, CompanyQueryCommand, CompanyQueryError, CompanyQueryService,
+        InmemoryCompanyQueryServiceImpl,
+    };
+    use domain::stock::StockId;
+
+    fn setup() -> impl CompanyQueryService {
+        let mut service = InmemoryCompanyQueryServiceImpl::new();
+        let sample_data = vec![
+            CompanyData {
+                name: "AGC".to_string(),
+                stock_id: StockId::new("1111".to_string()),
+                sector: "旅館".to_string(),
+                industry: "化学工業".to_string(),
+            },
+            CompanyData {
+                name: "KDDI".to_string(),
+                stock_id: StockId::new("2222".to_string()),
+                sector: "化学繊維製造業".to_string(),
+                industry: "化学工業".to_string(),
+            },
+            CompanyData {
+                name: "T&Dホールディングス".to_string(),
+                stock_id: StockId::new("3333".to_string()),
+                sector: "人材派遣業".to_string(),
+                industry: "鉱業".to_string(),
+            },
+            CompanyData {
+                name: "いすゞ自動車".to_string(),
+                stock_id: StockId::new("4444".to_string()),
+                sector: "製氷業".to_string(),
+                industry: "建設業".to_string(),
+            },
+            CompanyData {
+                name: "りそなホールディングス".to_string(),
+                stock_id: StockId::new("5555".to_string()),
+                sector: "化学繊維製造業".to_string(),
+                industry: "製造業".to_string(),
+            },
+        ];
+
+        service.companies = sample_data;
+
+        service
     }
 
     #[tokio::test]
-    async fn find_by_id_with_param() -> anyhow::Result<()> {
-        unimplemented!()
+    async fn find_by_name() -> anyhow::Result<()> {
+        let service = setup();
+        let mut command = CompanyQueryCommand::new();
+        command.name = Some("いすゞ自動車".to_string());
+
+        assert!(service.find(command).await?.pop().unwrap().name == "いすゞ自動車".to_string());
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn find_by_id_command() -> anyhow::Result<()> {
+        let service = setup();
+        let mut command = CompanyQueryCommand::new();
+        command.stock_id = Some("5555".to_string());
+
+        assert!(
+            service
+                .find(command)
+                .await?
+                .pop()
+                .unwrap()
+                .stock_id
+                .to_string()
+                == "5555".to_string()
+        );
+
+        Ok(())
     }
 
     #[tokio::test]
     async fn find_by_id() -> anyhow::Result<()> {
-        unimplemented!()
+        let service = setup();
+        assert!(
+            service
+                .find_by_id("4444".to_string())
+                .await?
+                .stock_id
+                .to_string()
+                == "4444".to_string()
+        );
+
+        Ok(())
     }
 
     #[tokio::test]
     async fn find_notexist_id_return_err() -> anyhow::Result<()> {
-        unimplemented!()
+        let service = setup();
+        let Err(CompanyQueryError::CompanyNotFound(_)) = service.find_by_id("not exits id".to_string()).await else {
+            return Err(anyhow!("unexpected stock query result"));
+        };
+
+        Ok(())
     }
 
     #[tokio::test]
     async fn find_by_sector() -> anyhow::Result<()> {
-        unimplemented!()
+        let service = setup();
+        let mut command = CompanyQueryCommand::new();
+        command.sector = Some("化学繊維製造業".to_string());
+
+        assert!(
+            service
+                .find(command.clone())
+                .await?
+                .pop()
+                .unwrap()
+                .sector
+                .to_string()
+                == "化学繊維製造業".to_string()
+        );
+        assert!(
+            service
+                .find(command)
+                .await?
+                .pop()
+                .unwrap()
+                .sector
+                .to_string()
+                == "化学繊維製造業".to_string()
+        );
+
+        Ok(())
     }
 
     #[tokio::test]
     async fn find_by_industry() -> anyhow::Result<()> {
-        unimplemented!()
+        let service = setup();
+        let mut command = CompanyQueryCommand::new();
+        command.industry = Some("化学工業".to_string());
+
+        assert!(
+            service
+                .find(command.clone())
+                .await?
+                .pop()
+                .unwrap()
+                .industry
+                .to_string()
+                == "化学工業".to_string()
+        );
+        assert!(
+            service
+                .find(command)
+                .await?
+                .pop()
+                .unwrap()
+                .industry
+                .to_string()
+                == "化学工業".to_string()
+        );
+
+        Ok(())
     }
 
     #[tokio::test]
     async fn pagenation() -> anyhow::Result<()> {
-        unimplemented!()
+        let mut service = setup();
+        let mut param = CompanyQueryCommand::new();
+        let index = Some(2);
+        let page_size = Some(1);
+        param.page = index;
+        param.size = page_size;
+
+        let found = service.find(param).await?;
+        assert!(found.len() as i32 == page_size.unwrap());
+        assert!(found[0].stock_id == StockId::new("2222".to_string()));
+
+        Ok(())
     }
 
     #[tokio::test]
     async fn find_list() -> anyhow::Result<()> {
-        unimplemented!()
+        let service = setup();
+        let id_list = vec!["1111".to_string(), "2222".to_string(), "3333".to_string()];
+        let result = service.find_list(id_list.clone()).await?;
+        let result_id_list: Vec<String> = result.into_iter().map(|c| c.stock_id.to_string()).collect();
+
+        assert!(result_id_list == id_list);
+
+        Ok(())
     }
 }
